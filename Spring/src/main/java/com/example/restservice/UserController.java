ca93c9cf602e3cc79d1c781b0ec858cdc9f1b319
Telvin <telvin.zhong@braze.com>
2021-07-26 11:46:55 -0700

    cleaned unused statements

--- Spring/src/main/java/com/example/restservice/UserController.java
+++ Spring/src/main/java/com/example/restservice/UserController.java
@@ -8,25 +8,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class UserController {
   @Autowired
   UserRepository repository;
 
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
-
-    //get HTTP request....
   @GetMapping("/dates")
   public ArrayList<String> getDates() {
     List<User> list = repository.findAll();
     ArrayList<String> userDates = new ArrayList<String>();
     for (int i = 0; i < 10; i++) {
-//      System.out.println(list.get(i).getDate());
       userDates.add(list.get(i).getDate());
     }
     return userDates;
+  }
+
+  @GetMapping("/segments")
+  public ArrayList<String> getSegments() {
+    List<User> list = repository.findAll();
+    ArrayList<String> userSegments = new ArrayList<String>();
+    for (int i = 0; i < 10; i++) {
+      userSegments.add(list.get(i).getSegments().toString());
+    }
+    return userSegments;
+  }
+
+  @GetMapping("/segments")
+  public ArrayList<String> getLastUpdate() {
+    List<User> list = repository.findAll();
+    ArrayList<String> userLastUpdate = new ArrayList<String>();
+    for (int i = 0; i < 10; i++) {
+      userLastUpdate.add(list.get(i).getLastUpdate());
+    }
+    return userLastUpdate;
   }
 }
