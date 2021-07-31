2a3ebc4b9a6cba48aaebf1bcf31a6ccf3c3bb148
yimanliu <yimanliu@yimanliudeAir.attlocal.net>
2021-07-31 07:36:56 -0700

    add create get update delete apis

--- Spring/src/main/java/com/example/restservice/User.java
+++ Spring/src/main/java/com/example/restservice/User.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
-import java.util.Arrays;
 
 @Data
 @Document(collection = "storyline")
--- Spring/src/main/java/com/example/restservice/UserController.java
+++ Spring/src/main/java/com/example/restservice/UserController.java
@@ -1,23 +1,47 @@
 package com.example.restservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
-  @Autowired
-  UserRepository repository;
+  private final UserRepository repository;
+
+  UserController(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @PostMapping("/create")
+  User createUser(@RequestBody User user) {
+    return repository.save(user);
+  }
 
   @GetMapping("/summary")
   public List<String> getSummary() {
     List<User> list = repository.findAll();
     List<String> summaryList = new ArrayList<String>();
     return summaryList;
+  }
+
+  @PutMapping("/user/{date}")
+  User updateUser(@RequestBody User user, @PathVariable String date) {
+
+    return repository.findById(date)
+            .map(employee -> {
+              employee.setCaloriesIdle(user.getCaloriesIdle());
+              employee.setSummary(user.getSummary());
+              return repository.save(user);
+            })
+            .orElseGet(() -> {
+              user.setId(date);
+              return repository.save(user);
+            });
+  }
+
+  @DeleteMapping("/user/{date}")
+  void deleteEmployee(@PathVariable String date) {
+    repository.deleteById(date);
   }
 }
