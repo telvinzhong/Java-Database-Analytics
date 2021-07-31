ace0efae19c576b3fec82de17913e42d55393625
yimanliu <yimanliu0722@gmail.com>
2021-07-31 10:39:48 -0700

    remove JSON

--- CS5500-Sprint/controller/src/main/java/ActivitySum.java
+++
@@ -1,7 +0,0 @@
-public class ActivitySum {
-    public static void main(String[] args) {
-        ConnectToDatabase connection = new ConnectToDatabase("20130209");
-        System.out.println("Activity total time: " + connection.getTotalTime("walking"));
-        System.out.println("Activity total calories: " + connection.getTotalCalories("walking"));
-    }
-}
